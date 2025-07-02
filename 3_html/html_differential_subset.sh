a4b8c291418fbf5f5e0ce64cea087262a0242151
lily <lily.yang@pirbright.ac.uk>
2025-07-02 14:29:46 +0100

    v1.11.7 update: add new generated clusters of T cell and NK cell groups into original results and conducted DA, DE

--- 3_html/html_differential_subset.sh
+++ 3_html/html_differential_subset.sh
@@ -12,7 +12,7 @@
 ## 10 hours
 #SBATCH --time=1-10:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=3
 #SBATCH --mem-per-cpu=50G
 
 ## Job Steps
@@ -30,10 +30,16 @@ cd /mnt/lustre/RDS-live/bioinformatics/analysis/scrna/proj/bsp_248/lily/code/bsp
 export work_dir="/mnt/lustre/RDS-ephemeral/bioinformatics/analysis/scrna/proj/bsp_248/lily/stages/"
 export bsp_num="bsp_248"
 export clustK="25"
-export sampSet="subset_innate_immune_cells"
+
 export sampDPI="both"
 echo "sampSet" $sampSet
 ## sleep 10
+
+# export sampSet="subset_innate_immune_cells"
+# cF="/mnt/lustre/RDS-ephemeral/bioinformatics/analysis/scrna/proj/bsp_248/lily/stages/step_differential_${sampSet}.html"
+# /mnt/lustre/RDS-ephemeral/bioinformatics/analysis/scrna/local/R-4.5.0/bin/R -e "rmarkdown::render('differential_lily_uniform.Rmd',  output_file=\"${cF}\")"
+
+export sampSet="subset_adaptive_immune_cells"
 cF="/mnt/lustre/RDS-ephemeral/bioinformatics/analysis/scrna/proj/bsp_248/lily/stages/step_differential_${sampSet}.html"
 /mnt/lustre/RDS-ephemeral/bioinformatics/analysis/scrna/local/R-4.5.0/bin/R -e "rmarkdown::render('differential_lily_uniform.Rmd',  output_file=\"${cF}\")"
 
